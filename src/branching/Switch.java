4bd54ac295b088bcd6ae2093e7d0340c34c18445
Kshatriya666 <62422177+Kshatriya666@users.noreply.github.com>
2020-04-15 21:19:46 +0800

    Switch

---
+++ src/branching/Switch.java
@@ -0,0 +1,25 @@
+package branching;
+
+public class Switch {
+    public static void main(String[] args) {
+        int hobby=5;
+        switch (hobby){
+            case 1:
+                System.out.println("Gaming");
+                break;
+            case 2:
+                System.out.println("Singing");
+                break;
+            case 3:
+                System.out.println("Dancing");
+                break;
+            case 4:
+                System.out.println("Watching Anime");
+                break;
+            case 5:
+                System.out.println("Drawing");
+                break;
+        }
+
+    }
+}
