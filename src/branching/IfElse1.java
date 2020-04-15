683f7be4426d048bc96f904bdc08a509ab35e5af
Kshatriya666 <62422177+Kshatriya666@users.noreply.github.com>
2020-04-15 21:01:54 +0800

    If Else part 1

---
+++ src/branching/IfElse1.java
@@ -0,0 +1,15 @@
+package branching;
+
+public class IfElse1 {
+    public static void main(String[] args) {
+        double hargaGundam = 500000;
+        double isiDompet = 5000;
+
+        if (isiDompet<hargaGundam){
+            System.out.println("Uang anda sedang kekurangan gizi");
+        }
+        else {
+            System.out.println("Happy Khilaf!");
+        }
+    }
+}
