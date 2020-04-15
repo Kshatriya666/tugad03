957244b28584e07284ee1abc21ae60118d247a2c
Kshatriya666 <62422177+Kshatriya666@users.noreply.github.com>
2020-04-15 20:35:48 +0800

    Binary Operator Arithmetic

--- src/operator/BinaryOperator.java
+++ src/operator/BinaryOperator.java
@@ -5,5 +5,11 @@ public class BinaryOperator {
     int SKS = 24;
     double IPK = 5;
 
-    
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+    }
 }
